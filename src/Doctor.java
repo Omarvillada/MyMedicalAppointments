70526b65873d27e7a1bb3f7f452746138b8b1604
Omar León <olvillada@gmail.com>
2021-06-14 16:47:04 -0500

    Agregando metodo constructor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,7 +1,16 @@
+import java.util.function.DoubleConsumer;
+
 public class Doctor {
     int id;
     String name;
     String speciality;
+
+    public Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
+    }
+    public Doctor(String name){
+        System.out.println("El nombre del Doctor asignado es: " + name);
+    }
 
     //Methods
     public void showName(){
