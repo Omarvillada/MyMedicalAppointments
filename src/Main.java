b71b271ec3be2f07c5c86f18c12de1b41fca2080
Omarvillada <olvillada@gmail.com>
2021-06-16 00:20:23 -0500

    Se agregaclase Patient

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
+import javax.print.Doc;
+
+import java.util.Date;
+
 import static  ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor  myDoctor = new Doctor("Gonzalo Arango");
-        myDoctor.name = "Omar Villada";
-        myDoctor.showName();
-        myDoctor.showId();
+        //showMenu();
+        Doctor myDoctor = new Doctor("Omar Villada", "Programación");
+        Doctor.Day allDays [] = Doctor.Day.values();
+        for (Doctor.Day d:allDays
+             ) {
+            System.out.println(d);
+        }
 
-        Doctor myDoctor2 = new Doctor("Eduardo Escobar");
-        myDoctor2.showId();
-        showMenu();
+
+        myDoctor.addAvaiableAppointment(new Date(),"12PM");
+        myDoctor.addAvaiableAppointment(new Date(),"2PM");
+        myDoctor.addAvaiableAppointment(new Date(),"3PM");
+
+        for (Doctor.AvaiableAppointment aA: myDoctor.getAvaiableAppointments()
+             ) {
+            System.out.println(aA.getDate() + " "+ aA.getTime());
+        }
+
+        Patient patient = new Patient("ALejandra", "alejandra@gmail.com");
+        patient.setWeight(60.0);
+        System.out.println(patient.getWeight());
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber());
     }
 
 
